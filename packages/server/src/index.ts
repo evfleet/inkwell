e14e0a88135dfcfd139f4752b28a3f6e1ed5f969
Evan Fleet <evfleet@gmail.com>
2024-01-09 18:00:59 -0500

    feat(server): setup basic websocket server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,31 @@
+import { WebSocket, WebSocketServer } from "ws";
+
 import { build } from "./app";
 
 const start = async () => {
   const app = await build();
   const port = 8081;
 
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
+  });
+
+  const wss = new WebSocketServer({ server });
+
+  wss.on("connection", (ws) => {
+    ws.on("error", console.error);
+
+    ws.on("message", (msg, isBinary) => {
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(msg, { binary: isBinary });
+        }
+      });
+    });
+
+    ws.on("close", () => {
+      console.log("Connection closed");
+    });
   });
 };
 
