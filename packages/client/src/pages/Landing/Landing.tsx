b10cdb47d7688be2c7b2e4d2002cb801077655f9
Evan Fleet <evfleet@gmail.com>
2024-01-10 12:24:16 -0500

    feat(client): setup basic websocket connection

--- packages/client/src/pages/Landing/Landing.tsx
+++ packages/client/src/pages/Landing/Landing.tsx
@@ -1,7 +1,30 @@
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
 export function Landing() {
+  const { readyState, sendJsonMessage } = useWebSocket("ws://localhost:8081");
+
+  const handleClickSendMessage = () => {
+    sendJsonMessage("ping");
+  };
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
   return (
     <div>
       <p>Landing</p>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to send 'Ping'
+      </button>
     </div>
   );
 }
