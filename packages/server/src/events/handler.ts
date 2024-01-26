707cef8f7ec6c8ac7bc2529521c3e08fcfce18b5
Evan Fleet <evfleet@gmail.com>
2024-01-26 15:22:05 -0500

    feat(server): connect redis to create/join rooms

--- packages/server/src/events/handler.ts
+++ packages/server/src/events/handler.ts
@@ -1,32 +1,78 @@
 import { Server, Socket } from "socket.io";
 
+import { redis } from "../config/redis.js";
 import { createUnusedId } from "../utils/id.js";
 
 export function handler(io: Server, socket: Socket) {
-  socket.on("room:create", () => {
-    const roomId = createUnusedId(io);
+  socket.on("disconnecting", async () => {
+    try {
+      for (const room of socket.rooms) {
+        if (room !== socket.id) {
+          const roomKey = `room:${room}`;
+          const roomState = await redis.get(roomKey);
 
-    socket.join(roomId);
+          if (!roomState) {
+            return;
+          }
 
-    io.to(socket.id).emit("room:create_success", {
-      payload: {
+          const { players } = JSON.parse(roomState);
+          const newPlayerList = players.filter(
+            (player: any) => player.id !== socket.id
+          );
+
+          if (newPlayerList.length === 0) {
+            return redis.del(roomKey);
+          }
+
+          io.to(room).emit("room:leave", {
+            payload: {
+              id: socket.id,
+              username: socket.data.username,
+            },
+          });
+        }
+      }
+    } catch (err) {
+      console.log("something went wrong");
+    }
+  });
+
+  socket.on("room:create", async (message) => {
+    try {
+      const { username } = message.payload;
+      const roomId = createUnusedId(io);
+      const gameState = {
         roomId,
-      },
-    });
+        players: [
+          {
+            id: socket.id,
+            username,
+          },
+        ],
+      };
+
+      await redis.set(`room:${roomId}`, JSON.stringify(gameState));
+
+      socket.data.username = username;
+      socket.join(roomId);
+
+      io.to(socket.id).emit("room:create_success", {
+        payload: {
+          roomId,
+        },
+      });
+    } catch (err) {
+      io.to(socket.id).emit("room:create_fail");
+    }
   });
 
   socket.on("room:join", (msg) => {
     const { code, username } = msg.payload;
 
-    console.log("attempt join");
-
     if (!io.sockets.adapter.rooms.get(code)) {
-      console.log("room doesnt exist");
-      io.to(socket.id).emit("room:join_jail", {});
-      return;
+      return io.to(socket.id).emit("room:join_jail", {});
     }
 
-    console.log("room exists, joining");
     socket.join(code);
     io.to(code).emit("room:join_success", {
       payload: {
